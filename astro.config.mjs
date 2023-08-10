361622ee1dbe2a99e27d3a0fa4e9fd0cadeba48a
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2023-08-10 02:31:02 +0100

    cloudflare adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,7 +17,5 @@ export default defineConfig({
     }
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare()
 });
