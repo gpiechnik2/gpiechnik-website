478b0514efd218e7bcc9656ae0df760a4f8765bc
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2023-08-31 20:29:18 +0100

    Rss path changed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET() {
     title: 'Grzegorz Piechnik - Performance testing and cybersecurity | Blog',
     description: 'My journey learning Performance testing and cybersecurity',
     site: 'https://gpiechnik.app',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./src/content/posts/*.md')),
     customData: `<language>en-us</language>`,
   });
 }
