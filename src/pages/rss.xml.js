2be006c68796d644eb9c0be1bc7c887b8edf2faa
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2023-08-31 20:37:07 +0100

    Rss added

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,23 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
-export async function GET() {
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const posts = await getCollection("posts")
   return rss({
     title: 'Grzegorz Piechnik - Performance testing and cybersecurity | Blog',
     description: 'My journey learning Performance testing and cybersecurity',
-    site: 'https://gpiechnik.com',
-    items: await pagesGlobToRssItems(import.meta.glob('./src/content/posts/*.md')),
+    site: context.site,
+    // items: await pagesGlobToRssItems(import.meta.glob('./src/content/posts/*.md')),
+    items: posts.map((post) => ({
+        link: `/posts/${post.slug}/`,
+        // Note: this will not process components or JSX expressions in MDX files.
+        content: sanitizeHtml(parser.render(post.body)),
+        ...post.data,
+      })),
     customData: `<language>en-us</language>`,
   });
 }
